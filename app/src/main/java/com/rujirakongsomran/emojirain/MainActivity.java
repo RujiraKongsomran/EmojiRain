890d6dc3a299c832c254da557fe22fa9c01b9076
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-07-31 21:28:33 +0700

    Write JAVA Code

--- app/src/main/java/com/rujirakongsomran/emojirain/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/emojirain/MainActivity.java
@@ -3,12 +3,41 @@ package com.rujirakongsomran.emojirain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.luolc.emojirain.EmojiRainLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EmojiRainLayout mContainer;
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContainer = (EmojiRainLayout) findViewById(R.id.mContainer);
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mContainer.addEmoji(R.drawable.emoji_1_3);
+                mContainer.addEmoji(R.drawable.emoji_2_3);
+                mContainer.addEmoji(R.drawable.emoji_3_3);
+                mContainer.addEmoji(R.drawable.emoji_4_3);
+                mContainer.addEmoji(R.drawable.emoji_5_3);
+
+                mContainer.startDropping();
+                mContainer.setPer(10);
+                mContainer.setDuration(7200);
+                mContainer.setDropDuration(2400);
+                mContainer.setDropFrequency(500);
+
+                mContainer.startDropping();
+            }
+        });
     }
 }
