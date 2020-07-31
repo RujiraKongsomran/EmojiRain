3baf8a053414eafde2f2b3be1ca1d3525dc31e93
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-07-31 21:55:06 +0700

    Change background

--- app/src/main/java/com/rujirakongsomran/emojirain/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/emojirain/MainActivity.java
@@ -11,7 +11,7 @@ import com.luolc.emojirain.EmojiRainLayout;
 public class MainActivity extends AppCompatActivity {
 
     private EmojiRainLayout mContainer;
-    private Button btnStart;
+    //private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +19,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mContainer = (EmojiRainLayout) findViewById(R.id.mContainer);
-        btnStart = (Button) findViewById(R.id.btnStart);
 
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mContainer.addEmoji(R.drawable.emoji_1_3);
-                mContainer.addEmoji(R.drawable.emoji_2_3);
-                mContainer.addEmoji(R.drawable.emoji_3_3);
-                mContainer.addEmoji(R.drawable.emoji_4_3);
-                mContainer.addEmoji(R.drawable.emoji_5_3);
+        mContainer.addEmoji(R.drawable.emoji_1_3);
+        mContainer.addEmoji(R.drawable.emoji_2_3);
+        mContainer.addEmoji(R.drawable.emoji_3_3);
+        mContainer.addEmoji(R.drawable.emoji_4_3);
+        mContainer.addEmoji(R.drawable.emoji_5_3);
 
-                mContainer.startDropping();
-                mContainer.setPer(10);
-                mContainer.setDuration(7200);
-                mContainer.setDropDuration(2400);
-                mContainer.setDropFrequency(500);
+        mContainer.stopDropping();
+        mContainer.setPer(10);
+        mContainer.setDuration(50000);
+        mContainer.setDropDuration(2400);
+        mContainer.setDropFrequency(500);
 
-                mContainer.startDropping();
-            }
-        });
+        mContainer.startDropping();
+        //btnStart = (Button) findViewById(R.id.btnStart);
+
+//        btnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mContainer.addEmoji(R.drawable.emoji_1_3);
+//                mContainer.addEmoji(R.drawable.emoji_2_3);
+//                mContainer.addEmoji(R.drawable.emoji_3_3);
+//                mContainer.addEmoji(R.drawable.emoji_4_3);
+//                mContainer.addEmoji(R.drawable.emoji_5_3);
+//
+//                mContainer.startDropping();
+//                mContainer.setPer(10);
+//                mContainer.setDuration(7200);
+//                mContainer.setDropDuration(2400);
+//                mContainer.setDropFrequency(500);
+//
+//                mContainer.startDropping();
+//            }
+//        });
     }
 }
